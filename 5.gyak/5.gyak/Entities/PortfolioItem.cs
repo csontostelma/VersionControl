2adb1d63ec87ec54f2de3b445867a458a5b433fd
Telma Erika Csontos <telma.csontos@stud.uni-corvinus.hu>
2020-10-07 18:27:16 +0200

    Portfólió érték kiszámítása

---
+++ 5.gyak/5.gyak/Entities/PortfolioItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.gyak.Entities
+{
+    public class PortfolioItem
+    {
+        public string Index { get; set; }
+        public string Volume { get; set; }
+    }
+}
