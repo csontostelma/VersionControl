5747c2c40059657977847240e4fcf32629866673
Telma Erika Csontos <telma.csontos@stud.uni-corvinus.hu>
2020-10-11 09:57:36 +0200

    Adatok betöltése

--- 5.gyak/5.gyak/Form1.cs
+++ 5.gyak/5.gyak/Form1.cs
@@ -15,7 +15,7 @@ namespace _5.gyak
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
-        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+      
 
         public Form1()
         {
@@ -23,29 +23,11 @@ namespace _5.gyak
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
 
-            CreatePortfolio();
-        }
+        
 
-        private void CreatePortfolio()
-        {
-            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
-            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
-            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
-
-            dataGridView2.DataSource = Portfolio;
-        }
-
-        private decimal GetPortfolioValue(DateTime date)
-        {
-            decimal value = 0;
-            foreach (var item in Portfolio)
-            {
-                var last = (from x in Ticks
-                            where item.Index == x.Index.Trim()&& date <= x.TradingDay
-                            select x).First();
-                value += (decimal)last.Price*item.Volume;
-            }
-            return value;
         }
     }
+
+
+
 }
