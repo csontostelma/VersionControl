e65ff207a56eb0d720f823fb9996e2c315972c8b
Telma Erika Csontos <telma.csontos@stud.uni-corvinus.hu>
2020-10-15 13:12:39 +0200

    XML feldolgozása

--- Webszolgáltatás/Webszolgáltatás/Form1.cs
+++ Webszolgáltatás/Webszolgáltatás/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Webszolgáltatás.Entities;
 using Webszolgáltatás.MnbServiceReference;
 
@@ -40,6 +41,29 @@ namespace Webszolgáltatás
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateDate();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit=decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit!=0)
+                {
+                    rate.Value = value / unit;
+                }
+
+            }
         }
     }
 }
