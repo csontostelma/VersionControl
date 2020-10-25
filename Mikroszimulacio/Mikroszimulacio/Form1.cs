246517347fe1967dbec1a3477129c9eb05099148
Telma Erika Csontos <telma.csontos@stud.uni-corvinus.hu>
2020-10-25 12:40:15 +0100

    Adatok betöltése

--- Mikroszimulacio/Mikroszimulacio/Form1.cs
+++ Mikroszimulacio/Mikroszimulacio/Form1.cs
@@ -18,8 +18,6 @@ namespace Mikroszimulacio
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
-        Random rng = new Random(1234);
-
         public Form1()
         {
             InitializeComponent();
