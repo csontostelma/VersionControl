734ace149893a36a8e0fa8a9154616b880827759
Telma Erika Csontos <telma.csontos@stud.uni-corvinus.hu>
2020-11-15 20:10:20 +0100

    Absztrakció felhasználása a futószalagban

--- Fejlesztesi_mintak/Fejlesztesi_mintak/Abstractions/Toy.cs
+++ Fejlesztesi_mintak/Fejlesztesi_mintak/Abstractions/Toy.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Fejlesztesi_mintak.Abstractions
+namespace Fejlesztesi_mintak.Entities
 {
     public abstract class Toy: Label
     {
