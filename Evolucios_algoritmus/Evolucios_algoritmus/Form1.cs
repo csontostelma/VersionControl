6a8107a01dcf7e949ba735dc932377ac7b9005a6
Telma Erika Csontos <telma.csontos@stud.uni-corvinus.hu>
2020-11-26 12:23:31 +0100

    Induló populáció felépítése

--- Evolucios_algoritmus/Evolucios_algoritmus/Form1.cs
+++ Evolucios_algoritmus/Evolucios_algoritmus/Form1.cs
@@ -15,12 +15,31 @@ namespace Evolucios_algoritmus
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+        
+
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
+
+            //gc.AddPlayer();
+            //gc.Start(true);
 
         }
     }
