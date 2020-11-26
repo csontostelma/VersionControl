5c63f9fb31fa6f3bbb121520dd9672851868d000
Telma Erika Csontos <telma.csontos@stud.uni-corvinus.hu>
2020-11-26 12:31:18 +0100

    Iterációs lépés felépítése

--- Evolucios_algoritmus/Evolucios_algoritmus/Form1.cs
+++ Evolucios_algoritmus/Evolucios_algoritmus/Form1.cs
@@ -29,7 +29,10 @@ namespace Evolucios_algoritmus
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            
+            //gc.AddPlayer();
+            //gc.Start(true);
+
+            gc.GameOver += Gc_GameOver;
 
             for (int i = 0; i < populationSize; i++)
             {
@@ -38,9 +41,24 @@ namespace Evolucios_algoritmus
 
             gc.Start();
 
-            //gc.AddPlayer();
-            //gc.Start(true);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
 
         }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+        }
+
+
+       
     }
 }
